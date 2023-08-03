ddb87b15243e5e7e5770ca8f86ae36abb1c84d6a
Igor Zupunski <ipatheword@live.com>
2023-08-03 13:48:36 +0200

    added calculating functions

--- src/components/BMICalculator/index.js
+++ src/components/BMICalculator/index.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function BMICalculator() {
+    const [height, setHeight] = useState("");
+    const [weight, setWeight] = useState("");
+    const [bmiResult, setBMIResult] = useState(null);
+
+    const handleHeightChange = (event) => {
+        setHeight(event.target.value);
+    };
+
+    const handleWeightChange = (event) => {
+        setWeight(event.target.value);
+    };
+
+    const calculateBMI = () => {
+        const heightInMeters = height / 100;
+        const bmi = weight / (heightInMeters * heightInMeters);
+        const roundedBMI = Math.round(bmi * 100) / 100;
+        setBMIResult(roundedBMI);
+    }
+
     return(
         <div>
             
