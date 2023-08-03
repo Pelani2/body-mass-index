4ed5a77b17459de1b9a2f8bf299ba11990faa071
Igor Zupunski <ipatheword@live.com>
2023-08-03 16:58:26 +0200

    styled inputs

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./input-styles.scss";
 
-const Input = ({ type, value, onChange }) => {
+const Input = ({ type, value, onChange, className }) => {
+    const classNames = `Input ${className || ""}`;
+
     return(
         <input 
             type={type}
             value={value}
             onChange={onChange}
+            className={classNames}
         />
     );
 };
@@ -14,7 +18,8 @@ const Input = ({ type, value, onChange }) => {
 Input.propTypes = {
     type: PropTypes.string,
     value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    onChange: PropTypes.func
+    onChange: PropTypes.func,
+    className: PropTypes.string
 };
 
 export default Input;
